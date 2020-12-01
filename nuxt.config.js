ba81e934b19d97de3065bb026c97d84fe7d8702e
Vojtech Havranek <vojtahavranek@gmail.com>
2020-12-01 09:29:21 +0100

    Just testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   mode: 'universal',
   /*
-   ** Headers of the page
+   ** Headers of the page //
    */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
